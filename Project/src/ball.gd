02f290c0995175a8d97048b0af99c3f06717f3d7
Jordan-Demske <jmdemske@outlook.com>
2021-09-02 14:37:18 -0400

    Removed extraneous assets and updated Level.scene tile map.

--- Project/src/ball.gd
+++ Project/src/ball.gd
@@ -1,4 +1,16 @@
 extends RigidBody2D
 
-func _draw():
-	draw_circle(Vector2.ZERO, $CollisionShape2D.shape.radius, Color.wheat)
+var angle = 0
+export var degrees_per_second = 200
+
+func _process(delta):
+	if Input.is_action_just_pressed("launch"):
+		var impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * 50
+		apply_impulse(Vector2.ZERO, impulse)
+	if Input.is_action_pressed("increase angle"):
+		angle -= degrees_per_second * delta
+		print(angle)
+	if Input.is_action_pressed("decrease angle"):
+		angle += degrees_per_second * delta
+		print(angle)
+		
